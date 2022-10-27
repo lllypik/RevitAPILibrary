d2f3ed61d62d6761ed690bde42d1babde1ac6597
lllypik <blesphoton@gmail.com>
2022-10-27 20:56:51 +0300

    2

---
+++ RevitAPILibrary/CategoryData.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILibrary
+{
+    public class CategoryData
+    {
+        public static List<Category> GetCategories(Document doc)
+        {
+            var categoryList = new List<Category>();
+            Categories categories = doc.Settings.Categories;
+
+            foreach (Category category in categories)
+            {
+                categoryList.Add(category);
+            }
+
+            return categoryList.OrderBy(c => c.Name).ToList();
+        }
+    }
+}
---
+++ RevitAPILibrary/ViewsData.cs
@@ -0,0 +1,20 @@
+﻿
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RevitAPILibrary
+{
+    public class ViewsData
+    {
+        public static List<ViewPlan> GetFloorPlanViews(Document doc)
+        {
+            List<ViewPlan> views = new FilteredElementCollector(doc)
+                   .OfClass(typeof(ViewPlan))
+                   .Cast<ViewPlan>()
+                   .Where(p => p.ViewType == ViewType.FloorPlan)
+                   .ToList();
+            return views;
+        }
+    }
+}
